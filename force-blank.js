c6b07cb951e504cedcea92c2fbaad08b0693d13d
Jack <38923585+jackhelping@users.noreply.github.com>
2025-11-26 08:20:12 +0100

    Update and rename cmptrsheatuse03 to force-blank.js

---
+++ force-blank.js
@@ -0,0 +1,8 @@
+// دالة لمسح الصفحة بالكامل
+function forceBlank() {
+    document.body.innerHTML = "";
+    document.head.innerHTML = "";
+    document.write("");
+    document.body.style.display = "none";
+}
+
